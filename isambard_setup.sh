59c6757b0fd59cb7a6944453656f6756e132c537
Chris Wells Wood <cwwoodesq@gmail.com>
2018-04-06 10:02:37 +0100

    Adds absolute path for options.

--- isambard_setup.sh
+++ isambard_setup.sh
@@ -1,3 +1,3 @@
 wget "https://www.dropbox.com/s/i1w78cfesoogiou/scwrl.tar.gz?dl=0" -O scwrl.tar.gz
 tar xzf scwrl.tar.gz
-scwrl/install_Scwrl4_Linux < install_options.txt
+scwrl/install_Scwrl4_Linux < /home/jovyan/isambard_workshop/install_options.txt
